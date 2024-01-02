767078e34af5641a23b7ac34866e99cc3cb436d3
darlyzinhoo_ <122948478+darlyzin@users.noreply.github.com>
2024-01-02 14:48:02 -0300

    Delete archive directory

--- archive/src/functions/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "../../../src/functions/utils/convert";
-export * from "../../../src/functions/utils/format";
-export * from "../../../src/functions/utils/math";
-export * from "../../../src/functions/utils/crash";
-export * from "../../../src/functions/utils/promises";
